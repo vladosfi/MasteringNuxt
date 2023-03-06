7928c334a40654afd6c8cf570ba478869a5a9b14
vladosfi <vladosfi@gmail.com>
2023-03-06 16:57:14 +0200

    Making App Robust by adding types

---
+++ error.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <div class="prose">
+        <h1>404</h1>
+        <p>The page doesn't exist!</p>
+      </div>
+    </NuxtLayout>
+  </div>
+</template>
