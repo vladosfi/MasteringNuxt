86c922639f57c0e2c916648cc35b5eeae9d26711
vladosfi <vladosfi@gmail.com>
2023-03-07 13:54:56 +0200

    Route Middleware

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -21,39 +21,41 @@ const course = useCourse();
 const route = useRoute();
 
 definePageMeta({
-  validate({ params }) {
+  middleware: function ({params}, from) {
     const course = useCourse();
     const chapter = course.chapters.find((chapter) => chapter.slug === params.chapterSlug);
 
+
     if (!chapter) {
-      throw createError({
-        statusCode: 404,
-        message: "Chapter not found",
-      });
+      return abortNavigation(
+        createError({
+          statusCode: 404,
+          message: "Chapter not found",
+        })
+      );
     }
 
     const lesson = chapter.lessons.find((lesson) => lesson.slug === params.lessonSlug);
 
     if (!lesson) {
-      throw createError({
-        statusCode: 404,
-        message: "Lesson not found",
-      });
+      return abortNavigation(
+        createError({
+          statusCode: 404,
+          message: "Lesson not found",
+        })
+      );
     }
-
-    return true;
   },
 });
+
 
 const chapter = computed(() => {
   return course.chapters.find((chapter) => chapter.slug === route.params.chapterSlug);
 });
 
-
 const lesson = computed(() => {
   return chapter.value.lessons.find((lesson) => lesson.slug === route.params.lessonSlug);
 });
-
 
 const title = computed(() => {
   return `${lesson.value.title} - ${course.title}`;
