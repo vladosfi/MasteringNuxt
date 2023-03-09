bdce15a0887ad5af4f60e07f133e314855377f51
vladosfi <vladosfi@gmail.com>
2023-03-09 17:33:55 +0200

    Fully Typing Our Endpoint

--- server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -1,10 +1,13 @@
+import { Course, Chapter, Lesson, LessonWithPath } from '~/types/course';
 import course from '~/server/courseData';
 
-export default defineEventHandler((event) => {
+course as Course;
+
+export default defineEventHandler((event): LessonWithPath => {
     const { chapterSlug, lessonSlug } = event.context.params;
 
 
-    const chapter = course.chapters.find((chapter) => chapter.slug === chapterSlug);
+    const chapter: Maybe<Chapter> = course.chapters.find((chapter) => chapter.slug === chapterSlug);
 
     if (!chapter) {
         throw createError({
@@ -13,7 +16,7 @@ export default defineEventHandler((event) => {
         })
     }
 
-    const lesson = chapter.lessons.find((lesson) => lesson.slug === lessonSlug);
+    const lesson: Maybe<Lesson> = chapter.lessons.find((lesson) => lesson.slug === lessonSlug);
 
     if (!lesson) {
         throw createError({
@@ -22,5 +25,8 @@ export default defineEventHandler((event) => {
         })
     }
 
-    return lesson;
+    return {
+        ...lesson,
+        path: `/course/chapter/${chapterSlug}/lesson/${lessonSlug}`
+    };
 })
